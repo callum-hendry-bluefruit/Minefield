f6dd7714fdcc6c917908f3c3e1cd241517fd576f
callum-hendry <callum.hendry@bluefruit.co.uk>
2017-06-19 13:21:43 +0100

    Passed

--- Minefield.cpp
+++ Minefield.cpp
@@ -55,14 +55,14 @@ int MineGrid::ReturnNumberOfAdjacentMines(const int x, const int y)
         m_currentSurroundings[1] = m_mineGrid[x][(y - 1)];
     }
 
-    /*if ((x + 1) <= (m_xSize - 1))
+    if ((x - 1) >= 0)
     { //Left
-       m_currentSurroundings[2] = m_mineGrid[(x + 1)][y]; 
+       m_currentSurroundings[2] = m_mineGrid[(x - 1)][y]; 
     }
 
-    if ((x - 1) >= 0)
+    /*if ((x + 1) <= (m_xSize - 1))
     { //Right
-        m_currentSurroundings[3] = m_mineGrid[(x - 1)][y];
+        m_currentSurroundings[3] = m_mineGrid[(x + 1)][y];
     }
 
     if (((y - 1) >= 0) && ((x - 1) >= 0))
